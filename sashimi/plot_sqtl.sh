ab1ad04e6fde63f494d7375021f3b622d444e334
Omar El Garwany <oe2@sanger.ac.uk>
2024-12-15 00:51:35 +0000

    testing and adding data

--- sashimi/plot_sqtl.sh
+++ sashimi/plot_sqtl.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
-#USAGE: bash ./plot_sqtl.sh 1200 20 0.01 ENSG00000175354.18 chr18 12812365 12832944 rs80262450 chr18:12818923 "clu_13508_-" $GTE_MAP "/lustre/scratch123/hgi/projects/macromapsqtl/oe2/output/realignedBam/" "_CIL_24.Aligned.sortedByCoord.waspFiltered.out.bam" "/lustre/scratch123/hgi/projects/macromapsqtl/oe2/output/tmp/sashimi/"
+#USAGE: bash ./plot_sqtl.sh 1200 20 0.01 ENSG00000175354.18 chr18 12812365 12832944 rs80262450 chr18:12818923 "$VCF" "clu_12004_-" ${intron_counts_file} $GTE_MAP "${rnaseq_bam_prefix}" "${rnaseq_bam_suffix}" "${genocode_gtf_f}" "${output_dir}"
 
+#TEST:
+# rnaseq_bam_prefix=/lustre/scratch123/hgi/projects/macromapsqtl/oe2/output/realignedBam/CIL_24/
+# rnaseq_bam_suffix=.Aligned.sortedByCoord.waspFiltered.out.bam
+# output_dir=/lustre/scratch123/hgi/projects/macromapsqtl/oe2/output/tmp/sashimi_public/
+# GTE_MAP=/lustre/scratch123/hgi/projects/macromapsqtl/oe2/output/misc/gte_mapping_CIL_24.txt
+# intron_counts_file=/lustre/scratch123/hgi/projects/macromapsqtl/oe2/output/intron_clustering/CIL_24/CIL_24_perind.counts_renamed
+# genocode_gtf_f=data/gencode.v46.annotation.gtf
 
 
 ######################################################
@@ -26,7 +33,7 @@ check_command "bcftools"
 ##########Checking command-line arguments#############
 ######################################################
 # Check if the required number of arguments is provided
-if [ "$#" -lt 15 ]; then
+if [ "$#" -lt 17 ]; then
   echo "Error: Missing arguments."
   echo "Usage: $0 <num_bins> <ylim_max> <scale_line_height> <gene> <chr> <s> <e> <snp> <clus> <gte_map> <rnaseq_bam_prefix> <rnaseq_bam_suffix> <output_dir>"
   exit 1
@@ -42,12 +49,14 @@ s="$6"
 e="$7"
 snp="$8"
 snp_pos="$9"
-$vcf_input_file="${10}"
+vcf_input_file="${10}"
 clus=("${11}")
-gte_map="${12}"
-rnaseq_bam_prefix="${13}"
-rnaseq_bam_suffix="${14}"
-output_dir="${15}"
+intron_counts_file="${12}"
+gte_map="${13}"
+rnaseq_bam_prefix="${14}"
+rnaseq_bam_suffix="${15}"
+gencode_gtf_f="${16}"
+output_dir="${17}"
 
 
 
@@ -78,8 +87,13 @@ check_empty_arg "gte_map" "$gte_map"
 check_empty_arg "rnaseq_bam_prefix" "$rnaseq_bam_prefix"
 check_empty_arg "rnaseq_bam_suffix" "$rnaseq_bam_suffix"
 check_empty_arg "output_dir" "$output_dir"
+check_empty_arg "intron_counts_file" "$intron_counts_file"
+check_empty_arg "gencode_gtf_f" "$gencode_gtf_f"
 
 # Display the values (optional for debugging)
+echo =======================================
+echo ===========INPUT PARAMS================
+echo =======================================
 echo "num_bins: $num_bins"
 echo "ylim_max: $ylim_max"
 echo "scale_line_height: $scale_line_height"
@@ -95,7 +109,9 @@ echo "gte_map: $gte_map"
 echo "rnaseq_bam_prefix: $rnaseq_bam_prefix"
 echo "rnaseq_bam_suffix: $rnaseq_bam_suffix"
 echo "output_dir: $output_dir"
-
+echo "intron_counts_file" "$intron_counts_file"
+echo "gencode_gtf_f" "$gencode_gtf_f"
+echo =======================================
 ######################################################
 ######################################################
 ######################################################
@@ -106,20 +122,20 @@ mkdir -p "$output_dir"
 
 #1.Getting genotypes
 vcf_output_file="$output_dir""$snp".txt
-bash ./get_genotypes.sh "$snp" "$snp_pos" "$vcf_input_file" "$vcf_output_file"
+bash scripts/get_genotypes.sh "$snp" "$snp_pos" "$vcf_input_file" "$vcf_output_file"
 echo Got genotypes...
 
+
 #2.Merging bams for each genotype group
-gs=(0 1 2); for g in "${gs[@]}"; do bash ./merge_bams.sh "${gte_map}" "$g" "$snp" "$chr" "$s" "$e" "${clus[@]}" "$output_dir" ${rnaseq_bam_prefix} ${rnaseq_bam_suffix}; done;
+gs=(0 1 2); for g in "${gs[@]}"; do bash ./merge_bams.sh "${gte_map}" "$g" "$snp" "$chr" "$s" "$e" "${clus[@]}" "${intron_counts_file}" "$output_dir" "${rnaseq_bam_prefix}" "${rnaseq_bam_suffix}"; done;
 
 
 #3.Generating track files (bigwig and links) for each genotype group (0,1,2)
-bash ./generate_track_file.sh "${output_dir}${snp}_${chr}_${s}_${e}_0.avg" "${output_dir}${snp}_${chr}_${s}_${e}_0.avg.bw" "${output_dir}${snp}_${chr}_${s}_${e}_0.links""${links_suffix}" "${output_dir}${snp}_${chr}_${s}_${e}_1.avg" "${output_dir}${snp}_${chr}_${s}_${e}_1.avg.bw" "${output_dir}${snp}_${chr}_${s}_${e}_1.links""${links_suffix}" "${output_dir}${snp}_${chr}_${s}_${e}_2.avg" "${output_dir}${snp}_${chr}_${s}_${e}_2.avg.bw" "${output_dir}${snp}_${chr}_${s}_${e}_2.links""${links_suffix}" "GENCODE" "$GENCODE_GTF_FILE" "$num_bins" "${ylim_max}" "${scale_line_height}" > "$output_dir""${snp}_${chr}_${s}_${e}".ini "${region_length}"
+bash scripts/generate_track_file.sh "${output_dir}${snp}_${chr}_${s}_${e}_0.avg" "${output_dir}${snp}_${chr}_${s}_${e}_0.avg.bw" "${output_dir}${snp}_${chr}_${s}_${e}_0.links""${links_suffix}" "${output_dir}${snp}_${chr}_${s}_${e}_1.avg" "${output_dir}${snp}_${chr}_${s}_${e}_1.avg.bw" "${output_dir}${snp}_${chr}_${s}_${e}_1.links""${links_suffix}" "${output_dir}${snp}_${chr}_${s}_${e}_2.avg" "${output_dir}${snp}_${chr}_${s}_${e}_2.avg.bw" "${output_dir}${snp}_${chr}_${s}_${e}_2.links""${links_suffix}" "GENCODE" "$gencode_gtf_f" "$num_bins" "${ylim_max}" "${scale_line_height}" > "$output_dir""${snp}_${chr}_${s}_${e}".ini "${region_length}"
 echo "Track file $output_dir${snp}_${chr}_${s}_${e}.ini"
 
 
 #4.Creating plot
 output_f="$output_dir""${gene}_${snp}_${chr}_${s}_${e}".pdf
-plot_cmd="pyGenomeTracks --tracks ${output_dir}${snp}_${chr}_${s}_${e}.ini --region ${chr}:${s}-${e} -t \"${chr}:${s}-${e} (sQTL = ${snp})\" --width 16 --trackLabelFraction 0.01 -out ${output_f} --fontSize 6"
-echo $plot_cmd
+pyGenomeTracks --tracks ${output_dir}${snp}_${chr}_${s}_${e}.ini --region ${chr}:${s}-${e} -t "${chr}:${s}-${e} (sQTL = ${snp})" --width 16 --trackLabelFraction 0.01 -out ${output_f} --fontSize 6
 printf "===========\nYou will find the pdf at:\n %s\n" "$output_f"
